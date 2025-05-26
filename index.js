291c03a2ad7456b48eeb89e1782097c2c1337079
alexandruiordan99 <alexandru.iordan77@gmail.com>
2025-05-26 12:47:26 +0200

    Now uses proper imports and tests multiple functions

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
-import {HashMap} from './hashmap.js';
+const {HashMap} = require("./hashmap.js");
 
 const test = new HashMap();
 
-test.set('apple', 'red');
+test.set('apple', 'red')
+test.set('banana', 'yellow')
+test.set('carrot', 'orange')
+test.set('dog', 'brown')
+test.set('elephant', 'gray')
+test.set('frog', 'green')
+test.set('grape', 'purple')
+test.set('hat', 'black')
+test.set('ice cream', 'white')
+test.set('jacket', 'blue')
+test.set('kite', 'pink')
+test.set('lion', 'golden')
+
+console.log(test.get('apple'));       // 'red'
+console.log(test.has('banana'));      // true
+console.log(test.keys());             // list of all keys
+console.log(test.values());           // list of all values
+console.log(test.entries());          // list of all [key, value] pairs
+console.log(test.length());           // number of items in the map
